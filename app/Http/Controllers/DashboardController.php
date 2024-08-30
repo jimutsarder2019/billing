a128d69c6bb2cea9c67d02fbe33fdd59b749506b
eliferahuldeb <rahul@elifetransfer.com>
2024-08-30 12:49:42 +0600

    update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -22,11 +22,60 @@ class DashboardController extends Controller
             $request['is_return'] = true;
             $accountcontroller = new Account();
             $monthly_report =  $accountcontroller->monthly_accounts($request);
-            if (auth()->user()->type == 'app_manager') {
+            
+			$data = null;
+            $mkt_data = null;
+            $mikrotik_id = '';
+            $status = '';
+            if ($request->has('username')) {
+                $data = Customer::with('mikrotik')->where('username', $request->username)->first();
+                if ($data) {
+                    $invoices = Invoice::where('customer_id', $data->id)->latest()->first();
+
+                    $connection = new ConnectionService($data->mikrotik->host, $data->mikrotik->username, $data->mikrotik->password, $data->mikrotik->port);
+                    $res_query = $connection->check_milrotiok_user_status($data->username);
+
+                    if (is_string($res_query)) return error_message($res_query);
+                    $status = $res_query['status'];
+                    $mikrotik_id = $data->mikrotik->id;
+                    if (isset($res_query['query']) && is_array($res_query['query'])) {
+                        foreach ($res_query['query'] as $key => $value) {
+                            $mkt_data['caller-id'] = $value['caller-id'] ?? '';
+                            $mkt_data['last-logged-out'] = $value['last-logged-out'] ?? '';
+                            $mkt_data['uptime'] = $value['uptime'] ?? '';
+                            $mkt_data['address'] = $value['address'] ?? '';
+                        }
+                    }
+                }
+            }
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			if (auth()->user()->type == 'app_manager') {
                 $today = Carbon::now()->format('d/m/Y H:i a');
                 $today_expiring_customers = Customer::where(['status' => 'active'])->where('expire_date', '<=', $today)->get()->count();
-                return view('content.dashboard.app-manager-dashboard', compact('today_expiring_customers', 'monthly_report'));
-            } elseif (auth()->user()->type == 'franchise') {
+                //return view('content.dashboard.app-manager-dashboard', compact('today_expiring_customers', 'monthly_report'));
+            
+			    return view('content.dashboard.app-manager-dashboard')->with([
+					'data' => $data ?? [],
+					'invoices' => $invoices ?? [],
+					'caller_id' => isset($mkt_data['caller-id']) ? $mkt_data['caller-id'] : '',
+					'last_logged_out' => isset($mkt_data['last-logged-out']) ? $mkt_data['last-logged-out'] : '',
+					'uptime' => isset($mkt_data['uptime']) ? $mkt_data['uptime'] : '',
+					'ip_address' => isset($mkt_data['address']) ? $mkt_data['address'] : '',
+					'status' => $status,
+					'mikrotik_id' => $mikrotik_id,
+					'today_expiring_customers' => $today_expiring_customers,
+					'monthly_report' => $monthly_report,
+				]);
+			
+			} elseif (auth()->user()->type == 'franchise') {
                 return view('content.dashboard.frinchise-dashboard', compact('monthly_report'));
             }
         } catch (\Throwable $th) {
