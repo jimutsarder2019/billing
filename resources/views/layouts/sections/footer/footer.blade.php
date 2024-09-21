e5ccc13d194f7a91071006d3332038fcce182161
eliferahuldeb <rahul@elifetransfer.com>
2024-09-21 11:51:26 +0600

    admin panel

--- resources/views/layouts/sections/footer/footer.blade.php
+++ resources/views/layouts/sections/footer/footer.blade.php
@@ -3,7 +3,7 @@
   <div class="{{ (!empty($containerNav) ? $containerNav : 'container-fluid') }}">
     <div class="footer-container d-flex align-items-center justify-content-between py-2 flex-md-row flex-column">
       <div>
-         <small>{{App\Models\AdminSetting::where('slug','copyright_right')->first() ? App\Models\AdminSetting::where('slug','copyright_right')->first()->value :'© copyright' }}{{ Illuminate\Support\Carbon::now()->format('Y')}}  |  Powerd By  <a href="{{ (!empty(config('variables.creatorUrl')) ? config('variables.creatorUrl') : '') }}" target="_blank" class="fw-semibold">Zubair IT</a></small>
+         <small>{{App\Models\AdminSetting::where('slug','copyright_right')->first() ? App\Models\AdminSetting::where('slug','copyright_right')->first()->value :'© copyright' }}{{ Illuminate\Support\Carbon::now()->format('Y')}}  |  Powerd By  <a href="{{ (!empty(config('variables.creatorUrl')) ? config('variables.creatorUrl') : '') }}" target="_blank" class="fw-semibold">ISP Manager</a></small>
       </div>
     </div>
   </div>
